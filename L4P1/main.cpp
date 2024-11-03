a6839eeace3444d0fc3473767295b4442c5de09b
Stefan <stefan.s.sarca@gmail.com>
2024-11-03 17:15:27 +0200

    l4

---
+++ L4P1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "perimetru:";
+    int p,l,a;
+    cin >>p;
+    l=p/4;
+    cout <<l <<endl;
+    a=l*l;
+    cout <<a;
+
+    return 0;
+}
--- L4P3/main.cpp
+++ L4P3/main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-     cout <<"Introdu nr:";
+    cout <<"Introdu nr:";
     int nr,u,z,s,result;
     cin >>nr;
     s=nr/100;
     z=nr%100/10;
     u=nr%10;
     cout <<s<<endl<<z<<endl<<u<<endl;
-    result=s+z+u;
+    result=s*10+u;
     cout <<result;
     return 0;
 }
